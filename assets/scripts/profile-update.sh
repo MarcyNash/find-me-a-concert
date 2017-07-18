f4d49fa92024fb30c72fc1684b9d2d62b97a62b4
MarcyNash <marcywsa@gmail.com>
2017-07-18 15:50:02 -0400

    Added authentication via AJAX calls to the server
    Copied auth from Where in the world is my recipe. Tested auth via
    CURL scripts. Then tested auth in browser.
    
    Tested profiles via CURL scripts.
    
    Note - Some Where In The World Is My Recipe UI may still be in
    index.html and auth/ui.js.

---
+++ assets/scripts/profile-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/profiles/${ID}"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+      "profile": {
+        "about_me": "'"${ABOUT}"'"
+      }
+  }'
+
+  # data output from curl doesn't have a trailing newline
+  echo
+
+  # allowed updates
+  # --data '{
+  #     "recipe": {
+  #       "location": "",
+  #       "comment": ""
+  #     }
